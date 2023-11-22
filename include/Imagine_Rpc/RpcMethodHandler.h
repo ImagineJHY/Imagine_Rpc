6f36e5a43d3323b8991f9d7cdb8a9d31d6ec46fe
Imagine <imaginejhy@163.com>
2023-11-22 18:13:48 +0800

    update

--- include/Imagine_Rpc/RpcMethodHandler.h
+++ include/Imagine_Rpc/RpcMethodHandler.h
@@ -45,10 +45,12 @@ Status RpcMethodHandler<RequestMessageType, ResponseMessageType>::HandleMethod(C
 {
     RequestMessageType* request_msg_type = dynamic_cast<RequestMessageType*>(request_msg);
     if (request_msg_type == nullptr) {
+        LOG_INFO("Request Message Type Error!");
         throw std::exception();
     }
     ResponseMessageType* response_msg_type = dynamic_cast<ResponseMessageType*>(response_msg);
     if (response_msg_type == nullptr) {
+        LOG_INFO("Response Message Type Error!");
         throw std::exception();
     }
 
