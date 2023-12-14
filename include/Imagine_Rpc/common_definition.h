4a8bf74d22911373fad99b741b856d842c4d78da
Imagine <imaginejhy@163.com>
2023-12-14 14:36:10 +0800

    update

--- include/Imagine_Rpc/common_definition.h
+++ include/Imagine_Rpc/common_definition.h
@@ -6,6 +6,9 @@
 namespace Imagine_Rpc
 {
 
+#define REGISTER_MEMBER_FUNCTION(METHOD_NAME, REQUEST_MESSAGE_TYPE, RESPONSE_MESSAGE_TYPE, METHOD_FUNCTION_NAME) RegisterMethods({METHOD_NAME}, {new ::Imagine_Rpc::RpcMethodHandler<REQUEST_MESSAGE_TYPE, RESPONSE_MESSAGE_TYPE>(std::bind(METHOD_FUNCTION_NAME, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3))})
+#define REGISTER_STATIC_FUNCTION(METHOD_NAME, REQUEST_MESSAGE_TYPE, RESPONSE_MESSAGE_TYPE, METHOD_FUNCTION_NAME) RegisterMethods({METHOD_NAME}, {new ::Imagine_Rpc::RpcMethodHandler<REQUEST_MESSAGE_TYPE, RESPONSE_MESSAGE_TYPE>(std::bind(METHOD_FUNCTION_NAME, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3))})
+
 enum class Status
 {
     OK,
