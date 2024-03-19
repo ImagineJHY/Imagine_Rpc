f5e20da82d7739decc1ec8aec9446370e10a5bf3
Imagine <imaginejhy@163.com>
2024-03-19 15:25:30 +0800

    update

--- proto_generate.sh
+++ proto_generate.sh
@@ -1,10 +1,13 @@
 cd proto
 
-protoc --cpp_out=. Context.proto
-protoc --cpp_out=. InternalMessage.proto
+protoc --cpp_out=. *.proto
 
-sed -i "s#Context.pb.h#Imagine_Rpc/Context.pb.h#g" Context.pb.cc
-sed -i "s#InternalMessage.pb.h#Imagine_Rpc/InternalMessage.pb.h#g" InternalMessage.pb.cc
+file_list=$(ls *.pb.cc)
+
+for filename in $file_list; do
+    filename_without_extension=$(basename $filename .pb.cc)
+    sed -i "s/$filename_without_extension.pb.h/Imagine_Rpc\/$filename_without_extension.pb.h/g" $filename
+done
 
 mv *.pb.h ../include/Imagine_Rpc/
 mv *.pb.cc ../src/
